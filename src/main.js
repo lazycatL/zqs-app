4a0b1c299099551934c760eecad4a5aebc262554
lazycatL <510754059@qq.com>
2018-01-15 17:13:08 +0800

    less通用变量

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './api/api'
-import ajax from './utils/ajax'
+import util from './utils/util'
+import Numbers from './utils/number'
 import './assets/css/reset.css'
 
 Vue.config.productionTip = false
-Vue.prototype.ajax = ajax
-Vue.prototype.api = api
+Vue.prototype.Api = api
+Vue.prototype.Util = util
+Vue.prototype.Number = Numbers
 
 /* eslint-disable no-new */
 new Vue({
